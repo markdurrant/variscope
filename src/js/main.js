468f6411a26d7be6ab84bb169751a6d946c07429
mark.j.durrant@gmail.com <Mark@Marks-MacBook-Pro.local>
2015-06-27 18:35:47 +0100

    add images + basic transitions

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
-(function () { 'use strict';
+function populateImages () {
+  $.getJSON( "data/itemList.json", function( json ) {
+    for (var i = 0; i < json.length; i++) {
+      if (i === json.length -1) {
+        $('.item-list').append('<li class="item active" style="background-image: url(../img/wiki/' + json[i].url + ');"></li>');
+      } else {
+        $('.item-list').append('<li class="item" style="background-image: url(../img/wiki/' + json[i].url + ');"></li>');
+      }
+    };
+  });
+}
 
-}()); // end 'use strict'
+function nextItem () {
+  $('.item.active').removeClass('active').prev().addClass('active');
+}
+
+function scrollChecker () {
+  $(window).scroll(function() {
+    if ($(window).scrollTop() % 50 === 0) {
+      nextItem();
+    };
+  });
+}
+
+populateImages();
+scrollChecker();
