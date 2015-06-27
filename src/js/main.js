5b6e3d4d398407bcce2c6dca375a78f862de77c1
mark.j.durrant@gmail.com <Mark@Marks-MacBook-Pro.local>
2015-06-27 20:02:21 +0100

    use real images

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,19 @@
-// function populateImages () {
-//   $.getJSON( "data/itemList.json", function( json ) {
-//     for (var i = 0; i < json.length; i++) {
-//       if (i === json.length -1) {
-//         $('.item-list').append('<li class="item active" style="background-image: url(../img/wiki/' + json[i].url + ');"></li>');
-//       } else {
-//         $('.item-list').append('<li class="item" style="background-image: url(../img/wiki/' + json[i].url + ');"></li>');
-//       }
-//     }
-//   });
-// }
+function populateImages () {
+  $.getJSON( "data/itemList.json", function( json ) {
+    for (var i = 0; i < json.length; i++) {
+      if (i === 4) {
+        $('.item-list').append('<li class="item current" style="background-image: url(../img/wiki/' + json[i].url + ');"></li>');
+      } else {
+        $('.item-list').append('<li class="item" style="background-image: url(../img/wiki/' + json[i].url + ');"></li>');
+      }
+    }
+  });
+}
 
-// function nextItem () {
-//   $('.item.active').removeClass('active').prev().addClass('active');
-// }
+populateImages();
 
-// function scrollChecker () {
-//   $(window).scroll(function() {
-//     if ($(window).scrollTop() % 50 === 0) {
-//       nextItem();
-//     }
-//   });
-// }
-
-// populateImages();
-// scrollChecker();
-
-//===============================================================================
-// TEST
-//===============================================================================
 function nextItem () {
-  var currentItem = $('.test-item.current'),
+  var currentItem = $('.item.current'),
       previousItem = currentItem.prev();
 
   currentItem.toggleClass('current prev');
@@ -44,7 +28,7 @@ function nextItem () {
 }
 
 function prevItem () {
-  var currentItem = $('.test-item.current'),
+  var currentItem = $('.item.current'),
       nextItem = currentItem.next();
 
   currentItem.toggleClass('current next2');
@@ -55,7 +39,7 @@ function prevItem () {
   nextItem.addClass('prev2');
   nextItem.one('animationend', function(e) {
     $(this).toggleClass('prev2 current');
-  })
+  });
 }
 
 $(window).keypress(function(e) {
@@ -67,21 +51,3 @@ $(window).keypress(function(e) {
     prevItem();
   }
 });
-
-
-
-// $(function() {
-//    $(window).keypress(function(e) {
-//        var ev = e || window.event;
-//        var key = ev.keyCode || ev.which;
-//        //do stuff with "key" here...
-//        var new_div = $('<div/>');
-//        new_div.hide();
-//        new_div.css('color', 'darkgreen');
-//        new_div.html('key code ' + key + ' was pressed!');
-//        $('body').append(new_div);
-//        new_div.fadeIn();
-//    });
-// });
-
-
