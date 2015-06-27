8f2399c59e3695f22d911705e071b6f7ff9b3535
mark.j.durrant@gmail.com <Mark@Marks-MacBook-Pro.local>
2015-06-27 19:24:45 +0100

    prototype transitions

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,51 @@
-function populateImages () {
-  $.getJSON( "data/itemList.json", function( json ) {
-    for (var i = 0; i < json.length; i++) {
-      if (i === json.length -1) {
-        $('.item-list').append('<li class="item active" style="background-image: url(../img/wiki/' + json[i].url + ');"></li>');
-      } else {
-        $('.item-list').append('<li class="item" style="background-image: url(../img/wiki/' + json[i].url + ');"></li>');
-      }
-    };
-  });
-}
+// function populateImages () {
+//   $.getJSON( "data/itemList.json", function( json ) {
+//     for (var i = 0; i < json.length; i++) {
+//       if (i === json.length -1) {
+//         $('.item-list').append('<li class="item active" style="background-image: url(../img/wiki/' + json[i].url + ');"></li>');
+//       } else {
+//         $('.item-list').append('<li class="item" style="background-image: url(../img/wiki/' + json[i].url + ');"></li>');
+//       }
+//     }
+//   });
+// }
 
+// function nextItem () {
+//   $('.item.active').removeClass('active').prev().addClass('active');
+// }
+
+// function scrollChecker () {
+//   $(window).scroll(function() {
+//     if ($(window).scrollTop() % 50 === 0) {
+//       nextItem();
+//     }
+//   });
+// }
+
+// populateImages();
+// scrollChecker();
+
+//===============================================================================
+// TEST
+//===============================================================================
 function nextItem () {
-  $('.item.active').removeClass('active').prev().addClass('active');
-}
+  var currentItem = $('.test-item.current');
 
-function scrollChecker () {
-  $(window).scroll(function() {
-    if ($(window).scrollTop() % 50 === 0) {
-      nextItem();
-    };
+  currentItem.toggleClass('current prev');
+
+  currentItem.one('animationend',
+    function(e) {
+
+    $(this).removeClass('current prev');
+
+  });
+
+  currentItem.prev().addClass('next');
+
+  currentItem.prev().one('animationend',
+    function(e) {
+
+    $(this).toggleClass('next current');
+
   });
 }
-
-populateImages();
-scrollChecker();
