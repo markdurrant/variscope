79bda1f31dcd7ac139757a54ccdc167bd3731f99
Mark Durrant <mark.j.durrant@gmail.com>
2015-06-27 23:50:53 +0100

    add simple x-ray scrapper

--- wikiScraper.js
+++ wikiScraper.js
@@ -1,55 +1,12 @@
-// import dependancy
-var express = require('express'),
-         fs = require('fs'),
-    request = require('request'),
-    cheerio = require('cheerio');
+var Xray = require('x-ray');
+var x = Xray();
 
-var app = express();
+var category = 'Electron_microscope_images';
 
-app.get('/scrape', function(req, res) {
-
-  // URL to hit
-  url = 'https://news.ycombinator.com/';
-
-  // make a request check for errors
-  request(url, function(error, response, html) {
-
-    // check for errors
-    if(!error){
-
-      // use cheerio
-      var $ = cheerio.load(html);
-
-      // init the things to scrape
-      var title, url;
-      var json = { title: "", url: "" };
-
-      // set a jquery elm to sort on
-      $('.title').filter(function(){
-        // store what comes back
-        var data = $(this);
-
-        // grab the text
-        title = data.children($('.post_title')).text();
-        url = data.children($('.sitebit')).text();
-
-        // store the text as json
-        json.title = title;
-        json.url = url;
-
-        console.log(title, url);
-      });
-    }
-  })
-
-  fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-
-    console.log('File successfully written! - Check your project directory for the output.json file');
-  })
-});
-
-app.listen('8081');
-
-console.log('something magic 8081');
-
-exports = module.exports = app;
+x('https://commons.wikimedia.org/wiki/Category:' + category, '.thumb', [{
+  altText: 'img@alt',
+  url: 'img@src'
+}])
+  .paginate('.next_page@href')
+  .limit(3)
+  .write(category + '.json')
